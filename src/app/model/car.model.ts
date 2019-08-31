4ecacb66161cc49cef75c76b9f47cb17e1569325
eduardoarakaki <eduardo.arakaki@gmail.com>
2019-08-30 23:25:05 -0300

    Add generic resource

--- src/app/model/car.model.ts
+++ src/app/model/car.model.ts
@@ -1,2 +1,15 @@
-export class Car {
+import { BaseResourceModel } from '../shared/resource/base-resource.model';
+
+export class Car extends BaseResourceModel {
+
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string) {
+        super();
+    }
+
+    static fromJson(jsonData: any): Car {
+        return Object.assign(new Car(), jsonData);
+    }
 }
---
+++ src/app/shared/resource/base-resource.model.spec.ts
@@ -0,0 +1,7 @@
+import { BaseResourceModel } from './base-resource.model';
+
+describe('BaseResource', () => {
+  it('should create an instance', () => {
+    expect(BaseResourceModel).toBeTruthy();
+  });
+});
